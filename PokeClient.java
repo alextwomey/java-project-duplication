490d0595c4425b061c8fa218a85f973c6296b950
Ethan Ruszanowski <ethanrusz@gmail.com>
2019-12-10 15:17:38 -0500

    Added trim

--- PokeClient.java
+++ PokeClient.java
@@ -22,14 +22,14 @@ public class PokeClient extends JFrame implements ActionListener {
   private JButton jbFight = new JButton("Fight");
   private JButton jbRun = new JButton("Run");
   private JButton jbSend = new JButton("Send");
-  
+
   //networking attributes
   private JTextPane myReadArea;
   private Socket s;
   private boolean connected = false;
   private String ipaddress;
   private String name;
-  private int PORT = 27015; 
+  private int PORT = 27015;
 
   // Output text area
   private JTextArea jtaOut = new JTextArea(2, 10);
@@ -61,7 +61,7 @@ public class PokeClient extends JFrame implements ActionListener {
          chatThread.start();
          }catch(Exception e){}
    }
-   
+
 	public void setupWindow() {
     JPanel jpSouth = new JPanel(new GridLayout(1, 2));
     JPanel jpRunFight = new JPanel(new GridLayout(1, 2));
@@ -150,38 +150,38 @@ public class PokeClient extends JFrame implements ActionListener {
    try{
       OutputStream out = s.getOutputStream();
       PrintWriter pout = new PrintWriter(out);
-      pout.println(name+": "+jtaMessageBox.getText());
+      pout.println(name+": "+jtaMessageBox.getText().trim());
       pout.flush();
-      String formatOut = String.format("\n%s",jtaMessageBox.getText());
+      String formatOut = String.format("\n%s",jtaMessageBox.getText().trim());
       jtaChat.setEditable(true);
       appendToPane(jtaChat, formatOut,Color.BLUE );
       jtaChat.setEditable(false);
       jtaMessageBox.setText("");
    }catch(Exception eee){}
-    
+
     //jtaChat.append("Me: " + jtaMessageBox.getText() + "\n");
     //jtaMessageBox.setText("");
     // TODO finish send method
   }
-  
+
    public class ThreadChatClient extends Thread{
       private String ipA;
       private String display;
-      
+
       public ThreadChatClient(String ip){
          ipA = ip;
-         
-         
+
+
       }
-      
-      
-      
-       
+
+
+
+
       public void run(){
          try{
             s = new Socket(ipA, PORT);
             connected = true;
-                   
+
          }catch(Exception e){}
          try{
             InputStream in = s.getInputStream();
@@ -203,21 +203,21 @@ public class PokeClient extends JFrame implements ActionListener {
             }catch(Exception e){}
          }
       }
-      
+
 
    }
    //AppendToPane method taken from internet, so i can change color of text in text pane
-      //this is the only code that i have taken from somewhere else and it is only for 
+      //this is the only code that i have taken from somewhere else and it is only for
       //visual purposes, I had it working with JTextArea initially but i wanted to add
       //color,  and you can see my JTextArea commented out.
    private void appendToPane(JTextPane tp, String msg, Color c)
       {
          StyleContext sc = StyleContext.getDefaultStyleContext();
          AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, c);
-      
+
          aset = sc.addAttribute(aset, StyleConstants.FontFamily, "Lucida Console");
          aset = sc.addAttribute(aset, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
-      
+
          int len = tp.getDocument().getLength();
          tp.setCaretPosition(len);
          tp.setCharacterAttributes(aset, false);
